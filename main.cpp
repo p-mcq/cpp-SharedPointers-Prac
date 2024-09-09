9af7aecd7c6e4578e7997300e34181fe3c897bdd
Paul McQuaid <104009739+p-mcq@users.noreply.github.com>
2024-09-09 12:21:33 +0200

    Refactor class A and class B to use shared_ptr and weak_ptr for better memory management

--- main.cpp
+++ main.cpp
@@ -7,19 +7,41 @@
 #include "Savings_Account.h"
 #include "Trust_Account.h"
 
-class Test
-{
-private:
-    int data;
+// So I don't have to write std:: everywhere
+using namespace std;
 
+class B; // Forward declaration
+
+class A
+{
+    shared_ptr<B> b_ptr; // Shared Pointer to B class
 public:
-    Test() : data{0} { std::cout << "Test Constructor (" << data << ")" << std::endl; }
-    Test(int _data) : data{_data} { std::cout << "Test Constructor (" << data << ")" << std::endl; }
-    int getData() const { return data; }
-    ~Test() { std::cout << "Test Destructor (" << data << ")" << std::endl; }
+    void setB(shared_ptr<B> &b)
+    {
+        b_ptr = b;
+    }
+    A() { cout << "A Constructor" << endl; }
+    ~A() { cout << "A Destructor" << endl; }
+};
+
+class B
+{
+    // shared_ptr<A> a_ptr; // Shared Pointer to A class - Should be weak_ptr
+    weak_ptr<A> a_ptr; // Weak Pointer to A class
+public:
+    void setA(shared_ptr<A> &a)
+    {
+        a_ptr = a;
+    }
+    B() { cout << "B Constructor" << endl; }
+    ~B() { cout << "B Destructor" << endl; }
 };
 
 int main()
 {
-   
+    // If kept the same as before, neither A nor B will be destructed since they are pointing to each other
+    shared_ptr<A> a = make_shared<A>();
+    shared_ptr<B> b = make_shared<B>();
+    a->setB(b);
+    b->setA(a);
 }
