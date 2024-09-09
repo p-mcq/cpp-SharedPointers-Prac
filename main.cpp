24d647ffaa3783786c1c07069664cccb169ea828
Paul McQuaid <104009739+p-mcq@users.noreply.github.com>
2024-09-09 14:43:48 +0200

    Using custom deleters in CPP with a brief look at lambda expressions

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "Savings_Account.h"
 #include "Trust_Account.h"
 
+using namespace std;
 class Test
 {
 private:
@@ -19,7 +20,26 @@ public:
     ~Test() { std::cout << "Test Destructor (" << data << ")" << std::endl; }
 };
 
+void myDeleter(Test *ptr)
+{
+    std::cout << "Using my custom function deleter" << std::endl;
+    delete ptr;
+}
+
 int main()
 {
-   
+    {
+        // Declaring a shared pointer with a customer deleter
+        std::shared_ptr<Test> ptr1{new Test{100}, myDeleter};
+    } // Once the scope ends, the shared pointer will be destroyed and the custom deleter will be called
+
+    {
+        // Declaring a shared pointer with a lambda function as a deleter
+        shared_ptr<Test> ptr2(new Test{100},
+                              [](Test *p)
+                              {
+                                  cout << "Using my custom lambda deleter" << endl;
+                                  delete p;
+                              });
+    }
 }
